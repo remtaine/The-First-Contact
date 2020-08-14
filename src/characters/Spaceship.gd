bfc2257804cf9e90405c44bb0d910c4c80d5473e
Benjo Kho <benjokho@gmail.com>
2020-08-14 14:28:52 +0800

    added asteroid field

--- src/characters/Spaceship.gd
+++ src/characters/Spaceship.gd
@@ -6,15 +6,18 @@ var is_flipped : bool = false
 onready var tween = $Tween
 onready var shot_cd = $Timers/ShotCooldown
 onready var bullet_point = $BulletPoint
-
 #onready var sound_hurt = preload("")
 
+onready var particles = $Particles
 onready var object_holder = get_parent().get_parent().get_node("Objects")
 onready var bullet_resource := preload("res://src/objects/Bullet.tscn")
 
 func _ready():
-	spawn_bullet()
-
+	set_physics_process(false)
+	#TODO do tween stuff
+	tween.interpolate_property(self, "scale", Vector2.ZERO, Vector2.ONE,0.5, Tween.TRANS_LINEAR,Tween.EASE_IN)
+	tween.start()
+	
 func change_direction(dir = "idle"):
 	if dir == "idle":
 #		play_sound("move")
@@ -30,3 +33,13 @@ func spawn_bullet():
 	var b = bullet_resource.instance()
 	b.setup(Vector2.UP, bullet_point.global_position)
 	object_holder.add_child(b)
+
+
+func _on_Tween_tween_completed(object, key):
+	print(key)
+	if key == ":scale":
+		play_sound("move")
+		yield(get_tree().create_timer(0.5), "timeout")
+		particles.visible = true
+		set_physics_process(true)
+		_on_ShotCooldown_timeout()
--- src/spawners/EnemySpawner.gd
+++ src/spawners/EnemySpawner.gd
@@ -1,6 +1,10 @@
 class_name EnemySpawner
 extends Node2D
 
+var monster_holder = []
+var starting_rows = 5
 
+onready var row_resource = preload("res://src/spawners/EnemyRowSpawner.tscn")
 func _ready():
-	pass
+	for i in range (0,starting_rows):
+		pass
